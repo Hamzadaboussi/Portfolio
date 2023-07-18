a06de606d3297b54e4f1d580bddb97c7f4a135e8
Hamzadaboussi <55136909+Hamzadaboussi@users.noreply.github.com>
2023-07-18 16:52:48 +0200

    Update HomeComponent.js

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Greeting from "../../containers/greeting/Greeting";
+import Highl from "../../containers/greeting/Highl";
 import Skills from "../../containers/skills/Skills";
 import Footer from "../../components/footer/Footer";
 
@@ -9,6 +10,7 @@ function Home(props) {
     <div>
       <Header theme={props.theme} setTheme={props.setTheme} />
       <Greeting theme={props.theme} />
+      <Highl theme={props.theme} />
       <Skills theme={props.theme} />
       <Footer theme={props.theme} />
     </div>
